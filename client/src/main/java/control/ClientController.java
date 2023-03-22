523b3f4fa9081fb9e5486a7e64e031ff81a8a817
Lőrincz Adrien-Jenő <lorinczadrienjeno@gmail.com>
2023-03-22 23:10:50 +0200

    Initial commit - no work

--- client/src/main/java/control/ClientController.java
+++ client/src/main/java/control/ClientController.java
@@ -1,4 +1,12 @@
 package control;
+
 public class ClientController {
-    
+    public ClientController() {
+        // Init GUI
+        GUIController guiController = new GUIController();
+    }
+
+    public static void main(String[] args) {
+        ClientController client = new ClientController();
+    }
 }
