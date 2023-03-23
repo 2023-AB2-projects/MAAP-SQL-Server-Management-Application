c769d252664ca8bd0ea8542dd3630ca360264971
akos <kovacsakos36@gmail.com>
2023-03-23 19:18:37 +0200

    valami

--- server/src/main/java/backend/config/Config.java
+++ server/src/main/java/backend/config/Config.java
@@ -6,8 +6,8 @@ import java.io.File;
 
 @Data
 public class Config {
-    private final String DB_CATALOG_PATH = System.getProperty("user.dir") + "/src/main/resources/Catalog.json";
-    private final String DB_CATALOG_ROOT = "Databases";
+    private static String DB_CATALOG_PATH = System.getProperty("user.dir") + "/src/main/resources/Catalog.json";
+    private static final String DB_CATALOG_ROOT = "Databases";
 
-    public File getCatalogFile() { return new File(DB_CATALOG_PATH); }
+    public static File getCatalogFile() { return new File(DB_CATALOG_PATH); }
 }
