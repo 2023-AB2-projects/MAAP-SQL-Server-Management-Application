52849fe464728cba5700227bcb039c7ce8c64b74
Lőrincz Adrien-Jenő <lorinczadrienjeno@gmail.com>
2023-03-23 15:54:14 +0200

    Initial commit - no work

--- server/src/main/java/backend/config/Config.java
+++ server/src/main/java/backend/config/Config.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class Config {
     private final String DB_CATALOG_PATH = System.getProperty("user.dir") + "/src/main/resources/Catalog.xml";
+    private final String DB_CATALOG_ROOT = "Databases";
 }
