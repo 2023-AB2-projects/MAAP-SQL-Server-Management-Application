43c0ecb2169cf1f389129f687a5a6d3192dcf0b8
akos <kovacsakos36@gmail.com>
2023-03-27 18:49:06 +0300

    finalize server init

--- server/src/main/java/backend/service/ServerController.java
+++ server/src/main/java/backend/service/ServerController.java
@@ -1,5 +1,6 @@
 package backend.service;
 
+import backend.ServerConnection;
 import backend.config.Config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -20,16 +22,33 @@ public class ServerController {
 
     @Getter
     @Setter
+    private String response;
+
+    @Getter
+    @Setter
     private String currentDatabase;
 
     @Getter
     @Setter
-    private List<String> databases;
+    private List<String> databaseNames;
+
+    @Getter
+    private final int port = 4444;
 
     public ServerController() {
         log.info("Server Started!");
 
+        // init catalog and set default database
         init();
+        try {
+            // start connection and send Catalog info to client
+            start(port);
+        } catch (IOException e) {
+            log.error("ServerController->start() : Connection error");
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     private void init() {
@@ -43,7 +62,10 @@ public class ServerController {
     }
 
     private void loadDatabases() {
-
+        databaseNames = new ArrayList<>();
+        databaseNames.add("master");
+        databaseNames.add("master2");
+        databaseNames.add("akoska");
     }
 
     private void accessCatalog() {
@@ -66,5 +88,49 @@ public class ServerController {
 
         Object jsonObject = mapper.readValue(jsonCreate, Object.class);
         mapper.writeValue(catalog, jsonObject);
+    }
+
+    public void start(int port) throws IOException {
+
+        ServerConnection serverConnection = new ServerConnection(port);
+
+        serverConnection.start();
+        log.info("Client Connected");
+        String shutdownMsg = "SHUTDOWN";
+
+        serverConnection.send(databaseNames.toString());
+
+        while(true){
+            try{
+                String msg = serverConnection.receive();
+
+                // check if client disconnected
+                if(msg.equals(shutdownMsg)){
+                    serverConnection.send("SERVER DISCONNECTED");
+                    serverConnection.stop();
+                    log.info("Server Shutting Down");
+                    break;
+                }
+
+                // pass message to parser and receive the answer
+                setSqlCommand(msg);         // if the client message is a sql command, then execute it
+                invokeParser();             // parse the command, and perform the db_actions
+
+                // build a response string, send to client
+                serverConnection.send(msg);
+
+
+            }catch (NullPointerException e){
+                serverConnection.stop();
+                log.info("Client Disconnected");
+                serverConnection.start();
+                log.info("Client Connected");
+            }
+        }
+
+        serverConnection.fullStop();
+    }
+
+    private void invokeParser() {
     }
 }
