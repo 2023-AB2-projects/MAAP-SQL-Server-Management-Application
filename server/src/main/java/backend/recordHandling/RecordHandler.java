4e217cb46e9b90622670b46c264767bbccefe0d4
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2023-04-10 19:13:01 +0300

    insert given line

--- server/src/main/java/backend/recordHandling/RecordHandler.java
+++ server/src/main/java/backend/recordHandling/RecordHandler.java
@@ -28,17 +28,33 @@ public class RecordHandler {
         tableStructure.add("long");
         tableStructure.add("char(10)");
 
-        recordSize = 0;
+        recordSize = 1;
         for (String type : tableStructure) {
-            System.out.println(sizeof(type));
             recordSize += sizeof(type);
         }
         io = new RandomAccessFile(fileLocation, "rw");
     }
 
-    public void insert(ArrayList<String> values, int line){
+    public void insert(ArrayList<String> values, int line) throws IOException {
+        if(values.size() != tableStructure.size()){
+            log.info("Wrong length of values");
+            return;
+        }
+        long offset = line * recordSize;
+        io.seek(offset);
+        if(io.length() > offset){
+            boolean b = io.readBoolean();
+            if(b){
+                log.info("Invalid location for write");
+                return;
+            }
+            io.seek(offset);
+        }
 
-        return;
+        io.writeBoolean(true);
+        for(int i = 0; i < values.size(); i++){
+            io.write(toBytes(tableStructure.get(i), values.get(i)));
+        }
     }
 
     public void close() throws IOException {
@@ -46,18 +62,13 @@ public class RecordHandler {
     }
 
     private long sizeof(String type){
-        switch (type){
-            case "int":
-                return Integer.BYTES;
-            case "long":
-                return Long.BYTES;
-            case "float":
-                return Float.BYTES;
-            case "double":
-                return Double.BYTES;
-            case "char":
-                return Character.BYTES;
-            default:
+        switch (type) {
+            case "int" -> { return Integer.BYTES; }
+            case "long" -> { return Long.BYTES; }
+            case "float" -> { return Float.BYTES; }
+            case "double" -> { return Double.BYTES; }
+            case "char" -> { return Character.BYTES; }
+            default -> {
                 Pattern pattern = Pattern.compile("char\\((\\d+)\\)");
                 Matcher matcher = pattern.matcher(type);
 
@@ -67,34 +78,55 @@ public class RecordHandler {
                 else{
                     return 0;
                 }
+            }
         }
     }
 
     private byte[] toBytes(String type, String value){
         ByteBuffer buffer;
-        switch (type){
-            case "int":
+        switch (type) {
+            case "int" -> {
                 buffer = ByteBuffer.allocate(Integer.BYTES);
                 buffer.putInt(Integer.parseInt(value));
-                return  buffer.array();
-            case "long":
+                return buffer.array();
+            }
+            case "long" -> {
                 buffer = ByteBuffer.allocate(Long.BYTES);
                 buffer.putLong(Long.parseLong(value));
-                return  buffer.array();
-            case "float":
+                return buffer.array();
+            }
+            case "float" -> {
                 buffer = ByteBuffer.allocate(Float.BYTES);
                 buffer.putFloat(Float.parseFloat(value));
-                return  buffer.array();
-            case "double":
+                return buffer.array();
+            }
+            case "double" -> {
                 buffer = ByteBuffer.allocate(Double.BYTES);
                 buffer.putDouble(Double.parseDouble(value));
-                return  buffer.array();
-            case "char":
+                return buffer.array();
+            }
+            case "char" -> {
                 buffer = ByteBuffer.allocate(Character.BYTES);
                 buffer.putChar(value.charAt(0));
-                return  buffer.array();
-            default:
-                return value.getBytes(StandardCharsets.US_ASCII);
+                return buffer.array();
+            }
+            default -> {
+                Pattern pattern = Pattern.compile("char\\((\\d+)\\)");
+                Matcher matcher = pattern.matcher(type);
+                if (matcher.find()) {
+                    long size = Long.parseLong(matcher.group(1));
+                    String formattedStr;
+                    if (size < value.length()) {
+                        formattedStr = value.substring(0, (int) size - 1);
+                    } else {
+                        formattedStr = String.format("%-" + size + "s", value);
+                    }
+
+                    return formattedStr.getBytes(StandardCharsets.US_ASCII);
+                } else {
+                    return new byte[0];
+                }
+            }
         }
     }
 }
--- server/src/main/java/backend/recordHandling/test.java
+++ server/src/main/java/backend/recordHandling/test.java
@@ -3,11 +3,18 @@ package backend.recordHandling;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Slf4j
 public class test {
     public static void main(String[] args) throws IOException {
         RecordHandler recordHandler = new RecordHandler("asd");
+        ArrayList<String> asd = new ArrayList<>();
+        asd.add("1");
+        asd.add("1.2");
+        asd.add("20");
+        asd.add("stange");
+        recordHandler.insert(asd, 1);
         recordHandler.close();
     }
 }
