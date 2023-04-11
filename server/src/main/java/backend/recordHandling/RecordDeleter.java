0a0cc252123f1012dd106f4f1438cfc6c55be413
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2023-04-11 20:42:47 +0300

    RecordDeleter

--- server/src/main/java/backend/recordHandling/RecordDeleter.java
+++ server/src/main/java/backend/recordHandling/RecordDeleter.java
@@ -1,15 +1,25 @@
 package backend.recordHandling;
 
+import backend.exceptions.recordHandlingExceptions.RecordNotFoundException;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RecordDeleter {
     private RecordHandler recordHandler;
+    private RecordFinder recordFinder;
     public RecordDeleter(String databaseName, String tableName) throws FileNotFoundException {
         recordHandler = new RecordHandler(databaseName, tableName);
     }
 
-    public void deleteByPrimaryKey(ArrayList<String> keyValues){
+    public void deleteByPrimaryKey(ArrayList<String> keyValues) throws RecordNotFoundException, IOException {
+        int line = recordFinder.findByPrimaryKey(keyValues);
+        recordHandler.deleteLine(line);
+    }
 
+    public void close() throws IOException {
+        recordFinder.close();
+        recordHandler.close();
     }
 }
