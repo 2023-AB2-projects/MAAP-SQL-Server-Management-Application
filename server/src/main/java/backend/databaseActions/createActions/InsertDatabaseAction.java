865bd4c5a04d681088dfc768323cec67c0efbe54
akos <kovacsakos36@gmail.com>
2023-04-18 09:36:12 +0300

    insert and deletedbaction 1/2

--- server/src/main/java/backend/databaseActions/createActions/InsertDatabaseAction.java
+++ server/src/main/java/backend/databaseActions/createActions/InsertDatabaseAction.java
@@ -2,10 +2,27 @@ package backend.databaseActions.createActions;
 
 import backend.databaseActions.DatabaseAction;
 import backend.exceptions.databaseActionsExceptions.*;
+import lombok.Setter;
+
+import java.util.List;
 
 public class InsertDatabaseAction implements DatabaseAction {
+    @Setter
+    private String databaseName;
+    @Setter
+    private String tableName;
+    @Setter
+    private List<String> columnNames;
+    @Setter
+    private Object [][] values;
+
+
     @Override
     public Object actionPerform() throws DatabaseNameAlreadyExists, TableNameAlreadyExists, DatabaseDoesntExist, PrimaryKeyNotFound, ForeignKeyNotFound, FieldCantBeNull, FieldsAreNotUnique, TableDoesntExist, IndexAlreadyExists, ForeignKeyFieldNotFound {
+        // the magic happens here...
+        // binaris file kezeles
+        // FIXME
+
         return null;
     }
 }
