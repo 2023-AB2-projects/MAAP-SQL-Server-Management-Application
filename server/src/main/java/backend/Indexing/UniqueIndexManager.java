30b4bfc539bf56329a07e27119abf4e62597907d
LorinczAdrien <lorinczadrienjeno@gmail.com>
2023-04-29 20:59:44 +0300

    Fixed hardcoded file and type handling

--- server/src/main/java/backend/Indexing/UniqueIndexManager.java
+++ server/src/main/java/backend/Indexing/UniqueIndexManager.java
@@ -1,12 +1,10 @@
 package backend.Indexing;
 
-import backend.config.Config;
 import backend.exceptions.recordHandlingExceptions.KeyAlreadyInTreeException;
 import backend.exceptions.recordHandlingExceptions.KeyNotFoundException;
-import backend.exceptions.recordHandlingExceptions.RecordNotFoundException;
 import backend.recordHandling.TypeConverter;
+import backend.service.CatalogManager;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -19,15 +17,10 @@ public class UniqueIndexManager {
     public UniqueIndexManager(String databaseName, String tableName, String indexName) throws IOException {
         this.databaseName = databaseName;
         this.tableName = tableName;
-        // keyStructure = CatalogManager.getIndexStructure(databaseName, tableName, indexName);
 
-        //remove later
-        keyStructure = new ArrayList<>();
-        keyStructure.add("int");
-
-        // String filename = CatalogManager.getIndexFileName(databaseName, tableName, indexName);
-        //remove later
-        String filename = Config.getDbRecordsPath() + File.separator + "test.index.bin";
+        // Index field types and file name
+        keyStructure = (ArrayList<String>) CatalogManager.getIndexFieldTypes(databaseName, tableName, indexName);
+        String filename = CatalogManager.getTableIndexFilePath(databaseName, tableName, indexName);
 
         bPlusTree = new BPlusTree(keyStructure, filename);
     }
