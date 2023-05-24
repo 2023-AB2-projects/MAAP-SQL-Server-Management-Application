0b1c647453017e5bc82bfc1e13552ef8611170e7
LorinczAdrien <lorinczadrienjeno@gmail.com>
2023-05-24 17:12:37 +0300

    Added icons

--- client/src/main/java/service/Config.java
+++ client/src/main/java/service/Config.java
@@ -8,11 +8,13 @@ import java.io.File;
 public class Config {
     private static final String DB_CATALOG_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "Catalog.json";
     private static final String DB_RECORDS_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "records";
+    private static final String IMAGES_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator + "images";
     private static final String DB_CATALOG_ROOT = "databases";
 
     /* Getters */
     public static String getDbCatalogPath() { return DB_CATALOG_PATH; }
     public static String getDbRecordsPath() { return  DB_RECORDS_PATH; }
+    public static String getImagesPath() { return IMAGES_PATH; }
     public static String getDbCatalogRoot() { return DB_CATALOG_ROOT; }
     public static File getCatalogFile() { return new File(DB_CATALOG_PATH); }
 }
